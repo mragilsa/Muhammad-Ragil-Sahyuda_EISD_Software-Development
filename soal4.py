336e63ba4f7080a02f1ea0f5fdd63f34d9a14334
Muhammad Ragil Sahyuda <142285719+mragilsa@users.noreply.github.com>
2025-04-04 20:31:42 +0700

    Update soal4.py

--- soal4.py
+++ soal4.py
@@ -1,17 +1,14 @@
+def cek_duplikat(input_angka):
+    data = list(map(float, input_angka.split(",")))  
+    angka_cek = []  
+
+    for angka in data:
+        if angka in angka_cek:
+            return True  
+        angka_cek.append(angka)  
+
+    return False 
+
 input_angka = input("Masukkan angka (pisahkan dengan koma): ")
-
-data = []
-for x in input_angka.split(","):
-    data.append(int(x)) 
-
-angka_cek = []
-
-for angka in data:
-    if angka in angka_cek:  
-        print("True")
-        break  
-    else:
-        angka_cek.append(angka) 
-
-else:
-    print("False")
+hasil = cek_duplikat(input_angka)
+print(hasil)
