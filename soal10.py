ddc4b7a69d6518c9417e9b976f4dc4329e612e0a
Muhammad Ragil Sahyuda <142285719+mragilsa@users.noreply.github.com>
2025-04-11 11:21:52 +0700

    Update soal10.py

--- soal10.py
+++ soal10.py
@@ -1,17 +1,19 @@
 pecahan = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100]
 
-total_pembayaran = int(input("Masukkan total pembayaran: "))
-total_belanja = int(input("Masukkan total belanja: "))
+total_pembayaran = int(input("Masukkan Total Pembayaran: "))
+total_belanja = int(input("Masukkan Total Belanja: "))
 
 kembalian = total_pembayaran - total_belanja
+hasil_kembalian = {}
 
 if kembalian == 0:
-    print("Tidak ada kembalian")
+    print("Tidak ada kembalian (uang sudah pas)")
 else:
-    print("Kembalian:")
+    for uang in pecahan:
+        if kembalian >= uang:
+            jumlah = kembalian // uang
+            hasil_kembalian[str(uang)] = jumlah
+            kembalian = kembalian % uang
 
-for uang in pecahan:
-    if kembalian >= uang:
-        jumlah = kembalian // uang  
-        print(f"{uang}: {jumlah}")  
-        kembalian = kembalian % uang  
+print("Kembalian:")
+print(hasil_kembalian)
